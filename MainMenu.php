2b82bbc2c1596401a344e5a11ff053a5f2ccb453
denis909 <denis909@mail.ru>
2020-05-05 23:39:09 +0300

    n/a

--- MainMenu.php
+++ MainMenu.php
@@ -56,4 +56,15 @@ namespace denis909\themes\tabler;
 */
 class MainMenu extends \denis909\yii\Menu
 {
+    
+    public $options = ['class' => 'nav nav-tabs border-0 flex-column flex-lg-row'];
+
+    public $itemOptions = ['class' => 'nav-item'];
+
+    public $activeCssClass = false;
+
+    public $activeLinkCssClass = 'active';
+
+    public $linkOptions = ['class' => 'nav-link'];
+
 }
