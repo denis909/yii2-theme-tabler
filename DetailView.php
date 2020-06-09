86c71d9bb4189485e8d86f4c719aa9faabc1ec8a
denis909 <denis909@mail.ru>
2020-06-09 20:15:05 +0300

    n/a

--- DetailView.php
+++ DetailView.php
@@ -7,4 +7,6 @@ class DetailView extends \denis909\bootstrap4\DetailView
 
     public $options = ['class' => 'table table-striped detail-view'];
 
+    public $template = '<tr><td{captionOptions}><div class="text-muted">{label}</div></td><td{contentOptions}>{value}</td></tr>';
+
 }
