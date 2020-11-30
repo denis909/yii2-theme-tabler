c5671effd40af6782ea71879a52984072d88206c
denis909 <denis909@mail.ru>
2020-11-30 12:02:04 +0300

    n/a

--- UserMenu.php
+++ UserMenu.php
@@ -2,26 +2,13 @@
 
 namespace denis909\themes\tabler;
 
-/*
-
-    <!--
-    <a class="dropdown-item" href="#">
-      <i class="dropdown-icon fe fe-user"></i> Profile
-    </a>
-    <a class="dropdown-item" href="#">
-      <i class="dropdown-icon fe fe-settings"></i> Settings
-    </a>
-    <a class="dropdown-item" href="#">
-      <span class="float-right"><span class="badge badge-primary">6</span></span>
-      <i class="dropdown-icon fe fe-mail"></i> Inbox
-    </a>
-    <a class="dropdown-item" href="#">
-      <i class="dropdown-icon fe fe-send"></i> Message
-    </a>
-    -->
-
-*/
-
 class UserMenu extends \denis909\theme\Menu
 {
+
+    public $options = ['tag' => false];
+    
+    public $itemOptions = ['tag' => false];
+
+    public $linkOptions = ['class' => 'dropdown-item'];
+
 }
